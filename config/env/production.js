abccc4e594e2cb03e331de257bade37d3ba6ff99
necavit <davmarod@gmail.com>
2016-03-05 16:53:19 +0100

    modified production environment configurator to use a non-tracked properties file to enhance security

--- config/env/production.js
+++ config/env/production.js
@@ -1,30 +1,24 @@
-// Exports an object 'config' with the appropriate values, retrieved in some cases
-//   from the process' environment. To do this, we evaluate a function (thus the
-//   '(function(args){ bla bla })' syntax - note the parenthesis around the funciton!)
-//   passing the arguments needed - process environment variables in this case.
-// The configurator fails and interrupts the process if some of the variables are not
-//   set, which doesn't mean that they're correct! ;)
-
-module.exports = (function (usr,pwd) {
+module.exports = (function () {
   var config = {};
 
+  // properties file not checked into Git!
+  var prod = require('./prod-properties.js');
+
   // check that the mongodb environment is correct
-  if (!usr) {
-    console.error('MONGO_USR environment variable not set');
+  if (!prod) {
+    console.error('Production properties file not loaded. Please check its existence and the values it has.');
     process.exit(1);
   }
-  if (!pwd) {
-    console.error('MONGO_PWD environment variable not set');
-    process.exit(1);
-  }
+
   // add the mongodb credentials to the connection URL
   config.mongodb = {};
-  config.mongodb.url =
-    'mongodb//'+usr+':'+pwd+'@ds055875.mongolab.com:55875/taskback'
+  config.mongodb.url = prod.mongo.uri
+    .replace('<dbuser>',prod.mongo.usr)
+    .replace('<dbpassword>'.prod.mongo.pwd);
 
   // set up the port to which the server will listen
-  config.port = 3000;
+  config.port = prod.port;
 
   // return the whole configuration object
   return config;
-})(process.env.MONGO_USR, process.env.MONGO_PWD);
+});
